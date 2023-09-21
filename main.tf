d12cf0e608ef951c41b6d0e71d1c087f28e46171
Wunmi <atiwunmi@yahoo.com>
2023-09-21 16:58:32 -0600

    backend

--- main.tf
+++ main.tf
@@ -1,3 +1,19 @@
-module "s3BU" {
-  source = "git@github.com:wumine2/s3-repo.git//s3-module?ref=v1.1.0"   
+module "s3" {
+  source = "git@github.com:wumine2/s3-repo.git//s3-module?ref=v1.2.0"
 }
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  backend "s3" {
+    bucket  = "bootcamp32-dev-1"
+    region  = "us-west-2"
+    key     = "action/terraform.tfstate"
+    encrypt = true
+  }
+}
+
